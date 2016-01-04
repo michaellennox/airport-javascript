ed44d005e394e103040477ad87071956480aa23f
michaellennox <michaelctlennox@gmail.com>
2016-01-04 18:51:05 +0000

    Moves comments

--- spec/features/UserStoriesSpec.js
+++ spec/features/UserStoriesSpec.js
@@ -6,15 +6,19 @@ describe('User Stories', function() {
     airport = new Airport();
   });
 
+  // As an air traffic controller
+  // So I can get passengers to a destination
+  // I want to instruct a plane to land at an airport and confirm that it has landed
   it('An airport traffic controller should be able to instruct a plane to land and receive confirmation', function() {
-    // As an air traffic controller
-    // So I can get passengers to a destination
-    // I want to instruct a plane to land at an airport and confirm that it has landed
     expect(plane.land(airport)).toEqual('Plane has successfully landed');
     expect(airport.planes).toContain(plane);
     expect(plane.isFlying).toBe(false);
   });
 
+
+  // As an air traffic controller
+  // So I can get passengers on the way to their destination
+  // I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
   it('An airport traffic controller should be able to instruct a plane to land and receive confirmation', function() {
     plane.land(airport);
     expect(plane.takeOff(airport)).toEqual('Plane has successfully taken off');
